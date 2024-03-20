0b46ec4de602891e238dac1274962388de183a25
Andrew Presman <alpresma@buffalo.edu>
2024-03-19 20:40:49 -0400

    created branch

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, flash, get_flashed_messages
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 
 @app.after_request
@@ -8,12 +9,56 @@ def add_header(response):
     return response
 
 @app.route('/')
-def playPage():
-    return send_from_directory('public', 'playPage.html')
+def index():
+    # return send_from_directory('templates', 'login.html')
+    return render_template('login.html')
 
 @app.route('/styles.css')
 def style():
-    return send_from_directory('public', 'styles.css')
+    return send_from_directory('static', 'styles.css')
+
+@app.route('/playstyles.css')
+def playstyle():
+    return send_from_directory('static', 'playstyles.css')
+
+
+users = {'username': 'password'}
+
+@app.route('/login_or_create', methods=['POST'])
+def login_or_create():
+
+  flash_messages = get_flashed_messages(with_categories=False, category_filter=['danger'])
+
+  for message in flash_messages:
+      flash(message, 'danger')
+
+  username = request.form['username']
+  password = request.form['password']
+  action = request.form['action']
+
+  if action == 'login':
+      if username in users and users[username] == password:
+          return redirect(url_for('dashboard'))
+      else:
+          flash('Login failed. Please check your username and password.', 'danger')
+          return redirect(url_for('index'))
+        
+  elif action == 'create_account':
+      if username not in users.keys():
+          users[username] = password
+          return redirect(url_for('dashboard'))
+      else:
+          flash('Username already taken. Please choose another username.', 'danger')
+          return redirect(url_for('index'))
+
+@app.route('/play_as_guest', methods=['POST'])
+def play_as_guest():
+    flash('Playing as Guest!', 'info')
+    return redirect(url_for('dashboard'))
+
+@app.route('/game')
+def dashboard():
+    return render_template('playPage.html')
 
 def main():
     host = "0.0.0.0"
@@ -21,7 +66,7 @@ def main():
 
     print("Listening on port " + str(port))
 
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
 
     
 if __name__ == "__main__":
