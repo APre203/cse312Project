19cf7c72b75997a975c54949ade17ef00a6f84b8
Andrew Presman <alpresma@buffalo.edu>
2024-03-22 02:25:08 -0400

    fixed chat section - need to add authentication and like functionality

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask, send_from_directory, render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, flash, get_flashed_messages, jsonify
+from util.db import *
+import json
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
@@ -10,7 +13,6 @@ def add_header(response):
 
 @app.route('/')
 def index():
-    # return send_from_directory('templates', 'login.html')
     return render_template('login.html')
 
 @app.route('/styles.css')
@@ -38,7 +40,7 @@ def login_or_create():
 
   if action == 'login':
       if username in users and users[username] == password:
-          return redirect(url_for('dashboard'))
+          return redirect(url_for('dashboard',name=username))
       else:
           flash('Login failed. Please check your username and password.', 'danger')
           return redirect(url_for('index'))
@@ -46,7 +48,7 @@ def login_or_create():
   elif action == 'create_account':
       if username not in users.keys():
           users[username] = password
-          return redirect(url_for('dashboard'))
+          return redirect(url_for('dashboard',name=username))
       else:
           flash('Username already taken. Please choose another username.', 'danger')
           return redirect(url_for('index'))
@@ -57,8 +59,34 @@ def play_as_guest():
     return redirect(url_for('dashboard'))
 
 @app.route('/game')
-def dashboard():
-    return render_template('playPage.html')
+@app.route('/game/<name>')
+def dashboard(name=None):
+    return render_template('playPage.html', name=name)
+
+
+@app.route('/chat/api',  methods=['GET', 'POST'])
+def add_chat_message():
+    if request.method == "GET":
+        # GET TOTAL FROM DATABASE
+        return jsonify(getallmessages())#[{"username": "test", "message": "something","id":1},{"username": "test2", "message": "something2","id":2}]), 200
+    data = request.json
+    print("data:",data)
+    username = "Guest"
+    auth = False
+    if auth:
+        username = "new_username"
+
+    message = data.get('message')
+  #  print(message)
+    
+    if message:
+        savechattod(username, message)
+        #chat_collection.insert_one({'username': username, 'message': message})
+        return jsonify({"username": username, "message": message,"id":1}), 201
+    else:
+        return jsonify({'error': 'Username and message are required'}), 400
+
+# NOW WE HAVE TO SEND ALL MESSAGES FROM MONGO WHEN WE RECEIVE THEM
 
 def main():
     host = "0.0.0.0"
---
+++ util/db.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+mongo_client = MongoClient("mongo")
+
+def savechattod(username, message):
+    db = mongo_client['ChatDB']
+    chat_collection = db['chat history']
+    chat_collection.insert_one({'username': username, 'message': message})
+
+def getallmessages():
+    db = mongo_client['ChatDB']
+    chat_collection = db['chat history']
+    retval = []
+    for record in chat_collection.find():
+        retval.append({"username":record["username"], "message":record["message"]})
+    return retval
