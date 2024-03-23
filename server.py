2f5a1d0f25c56caf184838b1b269952d55d44cbb
Aditya090202 <amathur7@buffalo.edu>
2024-03-22 23:21:37 -0400

    Added chat messaging through socketio

--- server.py
+++ server.py
@@ -1,11 +1,27 @@
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, render_template, request, redirect, send_from_directory
+from flask_socketio import SocketIO, emit, send
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.after_request
 def add_header(response):
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
+
+@socketio.on("message")
+def handle_message(message):
+    if message != 'User Connected!':
+        send(message, broadcast=True)
+
+
+@socketio.on('connect')
+def test_connect():
+    print("User Connected!")
+
+
 
 @app.route('/')
 def playPage():
