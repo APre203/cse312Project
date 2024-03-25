b980eee56169ab4fea197d2570bc72e8c8d5554e
ManavK003 <97770291+ManavK003@users.noreply.github.com>
2024-03-25 00:36:10 -0400

    Update db.py

--- util/db.py
+++ util/db.py
@@ -7,4 +7,11 @@ chat_collection = db["chat-history"]
 def storeMessage(username, msg):
     chat_collection.insert_one({username: msg})
 
+
+def getallmessages():
+    retval = []
+    for record in chat_collection.find():
+        retval.append({"username":record["username"], "message":record["message"]})
+    return retval
+
     
